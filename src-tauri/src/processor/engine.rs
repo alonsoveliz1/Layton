ecbc881a863570592549335bfdc8aa0ecda512ac
Alonso Veliz Garcia <alonsovelizgarcia12@gmail.com>
2025-09-09 12:27:18 +0200

    Computing final features when flow is terminated

--- src-tauri/src/processor/engine.rs
+++ src-tauri/src/processor/engine.rs
@@ -1,14 +1,13 @@
 use crossbeam_channel::{select, tick, Receiver, Sender};
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
-use std::collections::hash_map::Entry;
 
+use super::flow::{FlowDirection, FlowKey, FlowRecord, FLOW_TIMEOUT_US};
 use crate::capture::ParsedPacket;
 use crate::types::NetworkStats;
-use super::flow::{FlowKey, FlowRecord , FlowDirection, FLOW_TIMEOUT_US};
-
 
 #[inline]
 fn now_micros() -> u64 {
@@ -18,12 +17,17 @@ fn now_micros() -> u64 {
         .unwrap_or(0)
 }
 
-pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacket>, stats_tx: Sender<NetworkStats>, classifier_tx: Sender<FlowRecord>) {
+pub fn processing_loop(
+    running: Arc<AtomicBool>,
+    packet_rx: Receiver<ParsedPacket>,
+    stats_tx: Sender<NetworkStats>,
+    classifier_tx: Sender<FlowRecord>,
+) {
     let start_time = now_micros();
 
     // Timers to send expired flows to the classifier and stats to the frontend
     let expire_tick = tick(Duration::from_secs(1));
-    let stats_tick  = tick(Duration::from_secs(1));
+    let stats_tick = tick(Duration::from_secs(1));
 
     // We create the HashMap for the FlowRecords
     let mut flows: HashMap<FlowKey, FlowRecord> = HashMap::new();
@@ -39,7 +43,9 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
 
     loop {
         // While we're running
-        if !running.load(Ordering::Relaxed) { break; }
+        if !running.load(Ordering::Relaxed) {
+            break;
+        }
 
         select! {
             // We get the packets from the sniffer
@@ -58,8 +64,8 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
                                 FlowDirection::Forward
                             } else {
                                 FlowDirection::Backward
-                                
-                            };    
+
+                            };
                            // And insert the flow
                            let mut flow = FlowRecord::new(normalized_key, pkt.timestamp, first_direction);
                            e.insert(flow)
@@ -68,11 +74,11 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
 
                     // Check for flow termination (FIN flag)
                     let has_fin = pkt.tcp_flags & 0x01 != 0;
-                    
+
                     // Update flow features
                     flow.update_tcp_flow(
                         pkt.timestamp,
-                        pkt.flow_key.ip_a, pkt.flow_key.ip_b, 
+                        pkt.flow_key.ip_a, pkt.flow_key.ip_b,
                         pkt.flow_key.port_a, pkt.flow_key.port_b,
                         pkt.flow_key.protocol,
                         pkt.packet_len,
@@ -84,8 +90,11 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
 
                     // And send it to the classifier and remove it from the HashMap if should be removed
                     if flow.should_terminate(pkt.timestamp, has_fin) {
-                        // TODO SEND TO CLASSIFIER
-                        // flow.finalize();
+                        // Compute final features
+                        flow.calculate_derived_features();
+                        flow.finalize_for_export();
+
+                        // Get a copy to be sent to the classifier
                         let flow_copy = flow.clone();
                         flows.remove(&normalized_key);
                         let _ = classifier_tx.send(flow_copy);
@@ -94,7 +103,6 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
                     pkts_acc += 1;
                     total_pkts += 1;
                     bytes_acc += pkt.payload_len as u64;
-                    total_bytes += pkt.payload_len as i64;
                 }
             },
 
@@ -106,7 +114,11 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
                 flows.retain(|key, flow| {
                     let should_keep = (now - flow.last_seen_micros()) < FLOW_TIMEOUT_US;
                     if !should_keep{
-                        // flow.finalize(); Compute final features
+                        // Compute final features
+                        flow.calculate_derived_features();
+                        flow.finalize_for_export();
+
+                        // Add it to my vector of entries to be classified
                         flows_to_classify.push(flow.clone());
                     }
                     should_keep
@@ -127,7 +139,6 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
                     packets_per_second: (pkts_acc as f64) / dt,
                     bytes_per_second: (bytes_acc as f64) / dt,
                     total_packets: total_pkts,
-                    total_bytes: total_bytes,
                     uptime_seconds: ((now - start_time) / 1_000_000) as i64,
                 };
 
@@ -139,5 +150,3 @@ pub fn processing_loop(running: Arc<AtomicBool>, packet_rx: Receiver<ParsedPacke
         }
     }
 }
-
-
